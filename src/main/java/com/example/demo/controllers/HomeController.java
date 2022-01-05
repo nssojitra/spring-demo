3906711d314a61c71c35afaf4fa07d1cfde1fc5c
Nayan Sojitra <nssojitra@gmail.com>
2022-01-05 05:46:34 +0530

    Adding Index.html

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,15 +1,15 @@
 package com.example.demo.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 	
 	@GetMapping("")
 	public String Index()
 	{
-		return "<h1>Welcome to REST API</h1>";
+		return "index.html";
 	}
 
 }
